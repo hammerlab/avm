f49af849fdefbcd203bb79cefd2f2b8c42c7150a
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-03-16 14:06:54 -0400

    integrated RIC with obliteration into single outcome variable

--- data.py
+++ data.py
@@ -2,26 +2,86 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
+from numpy import nan as NaN
 
-def load_data(filename='AVM.xlsx'):
+FILENAME = 'AVM.xlsx'
+
+# some of the columns are poorly named in the original data file
+feature_rename_dict = {
+    'Max dose (Gy)': 'Max_Dose',
+    'age': 'Age',
+    'Volume (mL)': 'Volume',
+    'Associated ': 'Aneurysm',
+    'Peri_MarginalDose(Gy)': 'Marginal_Dose',
+    'Hx of H': 'History_of_Hemorrhage',
+    'No drainv vein': 'Number_Draining_Veins',
+    'S-M (vein)': 'SM_Vein',
+    'OP type': 'Surgery',
+    'Max D': 'Max_Diameter'
+}
+
+survival_outcome_rename_dict = {
+    'K-Mtime yrs': 'Years',
+}
+
+ric_rename_dict = {
+    'Degree of RIC': 'RIC_Degree',
+    'Permanet': 'RIC_Permanent',
+    'S/S from RIC': 'RIC_Symptoms',
+}
+
+def load_dataframe(filename=FILENAME):
     df = pd.read_excel(filename)
-    return create_datasets(df)
+    df = df.rename(
+        columns=survival_outcome_rename_dict).copy()
+    df = df.rename(
+        columns=feature_rename_dict).copy()
+    df = df.rename(
+        columns=ric_rename_dict).copy()
+    return df
+
+def load_datasets(filename=FILENAME):
+    df = load_dataframe(filename)
+    datasets = create_datasets(df)
+    return datasets, df
+
+
+def combine_output_variables(oblit, adverse):
+    """
+    Combine two boolean series, one for positive outcomes (obliteration)
+    and another for negative outcomes (adverse events), into a single
+    categorical variable.
+    """
+    only_oblit = (oblit > 0) & (adverse == 0)
+    # 0 = no obliteration, no adverse event
+    # 1 = adverse event (regardless of obliteration)
+    # 2 = obliteration, no adverse event
+    return 2 * only_oblit.astype(int) + (adverse > 0).astype(int)
+
 
 def create_datasets(df):
     # split the UVA and Pennsylvania datasets
     (_, uva), (_, pa) = df.groupby('CENTER')
     print("Extracting features for UVA")
-    X_uva, Y_uva, cols_uva = create_dataset(uva)
+    X_uva, Y_uva, oblit_uva, adverse_uva, df_uva = create_dataset(uva)
+
+    cols_uva = tuple(df_uva.columns)
+
     print("Extracting features for PA")
-    X_pa, Y_pa, cols_pa = create_dataset(pa)
-    assert cols_pa == cols_uva
+    X_pa, Y_pa, adverse_pa, oblit_pa, df_pa = create_dataset(pa)
+
+    cols_pa = tuple(df_pa.columns)
+    assert cols_pa == cols_uva, set(cols_pa).difference(set(cols_uva))
     return {
-        "uva" : (X_uva, Y_uva),
-        "pa" : (X_pa, Y_pa),
-        "features" : cols_pa
+        "uva": (X_uva, Y_uva, oblit_uva, adverse_uva, df_uva),
+        "pa": (X_pa, Y_pa, oblit_pa, adverse_pa, df_pa),
+        "features": cols_pa,
     }
 
-def create_dataset(center_df):
+def create_dataset(
+        center_df,
+        obliteration_years=4,
+        ric_years=4):
     """
     For each center, split its DataFrame into a set of features
     X and a label series Y.
@@ -34,7 +94,8 @@ def create_dataset(center_df):
         input_filter_mask.dtype,
         input_filter_mask.sum()))
 
-    Y_all_rows, followup_mask = survival_outcomes(center_df, obliteration_years=4)
+    oblit_all_rows, followup_mask = survival_outcomes(
+        center_df, obliteration_years=obliteration_years)
 
     print("-- Followup mask len=%d, type=%s, dtype=%s, sum=%d" % (
         len(followup_mask),
@@ -44,7 +105,7 @@ def create_dataset(center_df):
 
     combined_mask = input_filter_mask & followup_mask
     df_in_filtered = df_in_all_rows[combined_mask]
-    Y_filtered = Y_all_rows[combined_mask]
+    oblit_filtered = oblit_all_rows[combined_mask]
     print(
         "-- Total patients=%d"
         ", Complete patient rows=%d"
@@ -52,14 +113,18 @@ def create_dataset(center_df):
         ", Dataset size=%d" % (
             len(center_df),
             len(df_in_all_rows[input_filter_mask]),
-            len(Y_all_rows[followup_mask]),
-            len(Y_filtered)))
-    assert len(Y_filtered) <= len(Y_all_rows) <= len(center_df)
+            len(oblit_all_rows[followup_mask]),
+            len(oblit_filtered)))
+    assert len(oblit_filtered) <= len(oblit_all_rows) <= len(center_df)
     assert len(df_in_filtered) <= len(df_in_all_rows) <= len(center_df)
     X_filtered = np.array(df_in_filtered, dtype=float)
     print("X", X_filtered.shape, X_filtered.dtype)
-    print("Y", Y_filtered.shape, Y_filtered.dtype)
-    return X_filtered, Y_filtered, tuple(df_in_filtered.columns)
+    print("oblit", oblit_filtered.shape, oblit_filtered.dtype)
+
+    adverse_all = adverse_events(center_df)
+    adverse_filtered = adverse_all[combined_mask]
+    Y_filtered = combine_output_variables(oblit_filtered, adverse_filtered)
+    return X_filtered, Y_filtered, oblit_filtered, adverse_filtered, df_in_filtered
 
 # which columns of AVM.xlsx are we using as features for prediction of
 # of AVM obliteration
@@ -108,21 +173,17 @@ feature_columns = [
     'OP type'
 ]
 
-# some of the columns are poorly named in the original data file
-feature_rename_dict = {
-    'Max dose (Gy)' : 'Max_Dose',
-    'age' : 'Age',
-    'Volume (mL)' : 'Volume',
-    'Associated ' : 'Aneurysm',
-    'Peri_MarginalDose(Gy)' : 'Marginal_Dose',
-    'Hx of H' : 'History_of_Hemorrhage',
-    'No drainv vein' : 'Number_Draining_Veins',
-    'S-M (vein)' : 'SM_Vein',
-    'OP type' : 'Surgery',
-    'Max D' : 'Diameter'
-}
+# columns should be renamed when loaded from AVM excel file
+renamed_feature_columns = [
+    feature_rename_dict[f] if f in feature_rename_dict else f
+    for f in feature_columns
+]
 
-def extract_features(df, log_transform_age=True, expand_location_feature=True, dose_size_ratios=False):
+def extract_features(
+        df,
+        log_transform_age=True,
+        expand_location_feature=True,
+        dose_size_ratios=False):
     """
     Parameters
     ----------
@@ -140,8 +201,7 @@ def extract_features(df, log_transform_age=True, expand_location_feature=True, d
     Returns DataFrame subset of columns, along with boolean mask
     for filtering that DataFrame (and target values corresponding to each row)
     """
-    df_in = df[feature_columns].copy()
-    df_in.rename(columns = feature_rename_dict, inplace = True)
+    df_in = df[renamed_feature_columns].copy()
 
     # only care about surgery codes which include a resection
     surgery = df_in['Surgery']
@@ -156,9 +216,7 @@ def extract_features(df, log_transform_age=True, expand_location_feature=True, d
     # - Volume of AVM
     # - Embo: previous embolization
     bad_mask = np.zeros(len(df_in), dtype=bool)
-    for column_name in feature_columns:
-        if column_name in feature_rename_dict:
-            column_name = feature_rename_dict[column_name]
+    for column_name in renamed_feature_columns:
         column = df_in[column_name]
         missing = np.array(column.isnull())
         n_missing = missing.sum()
@@ -166,7 +224,6 @@ def extract_features(df, log_transform_age=True, expand_location_feature=True, d
             print("-- missing %d/%d feature values for %s" % (
                 n_missing, len(missing), column_name))
             bad_mask |= missing
-
 
     # expand Location feature after checking that it's not NaN
     if expand_location_feature:
@@ -205,9 +262,10 @@ survival_outcome_columns = [
     'K-Mtime yrs',
 ]
 
-survival_outcome_rename_dict ={
-    'K-Mtime yrs': 'Years',
-}
+renamed_survival_outcome_columns = [
+    'Years',
+    'Final_Result',
+]
 
 def survival_outcomes(df, obliteration_years=4.0):
     """
@@ -223,9 +281,7 @@ def survival_outcomes(df, obliteration_years=4.0):
     Returns target labels Y and mask for data points with sufficient followup.
     Using the series Y requires filtering by the followup mask.
     """
-    df_out = df[survival_outcome_columns].rename(
-        columns=survival_outcome_rename_dict).copy()
-
+    df_out = df[renamed_survival_outcome_columns].copy()
     # only counting total obliteration, as detected by either angiogram or MRI
     obliteration = (df_out['Final_Result'] == 4) | (df_out['Final_Result'] == 5)
 
@@ -247,49 +303,39 @@ def survival_outcomes(df, obliteration_years=4.0):
     return Y_all, sufficient_followup_mask
 
 ric_columns = [
+    # was there a radiation induced change?
     'RIC',
     'Degree of RIC',
+    # was the RIC permanent?
     'Permanet',
+    # symptoms categorization
     'S/S from RIC',
 ]
 
-ric_rename_dict = {
-    'Degree of RIC' : 'RIC_Degree',
-    'Permanet' : 'RIC_Permanent',
-    'S/S from RIC' : 'RIC_Symptoms',
-}
+renamed_ric_columns = [
+    'RIC',
+    'RIC_Degree',
+    'RIC_Permanent',
+    'RIC_Symptoms',
+]
 
 def radiation_induced_changes(df):
     """
     Normalized outcomes relating to radiation induced changes for each patient
     """
-    ric_df = df[ric_columns].rename(columns=ric_rename_dict).copy()
-    ric_df['RIC'] = ric_df['RIC'].convert_objects(convert_numeric=True)
 
-
-    ric_df['RIC_Symptoms'] = ric_df['RIC_Symptoms'].convert_objects(convert_numeric=True)
-    ric_df['RIC_Symptoms'][ric_df.RIC_Symptoms == 0] = NaN
-
-    # if only values should be False/True
-    ric_df['RIC_Permanent'] = ric_df['RIC_Permanent'].convert_objects(convert_numeric=True)
-    ric_df['RIC_Permanent'][ric_df['RIC_Permanent'].isnull()] = 0
-    ric_df['RIC_Permanent'] = ric_df['RIC_Permanent'].astype('bool')
-
-
+    ric_df = df[renamed_ric_columns].copy()
+    ric = ric_df['RIC'].convert_objects(convert_numeric=True)
+    ric[ric.isnull()] = 0
     # absence of data indicated either by 9 or NaN, normalize to only use one
-    ric_df['RIC'][ric_df['RIC'] == 9] = np.nan
-    ric_df['RIC_Degree'] = ric_df['RIC_Degree'].convert_objects(convert_numeric=True)
-    # if patient has an RIC and it's accompanied by permanent symptoms, that's bad!
-    ric_df['RIC_Bad'] = ((ric_df.RIC == 1) & ~ric_df.RIC_Symptoms.isnull() & ric_df.RIC_Permanent)
-
-    """
-    # drop rows without information about radiation induced changes
-        empty_ric =  ~(df['RIC'].str.strip().str.len().isnull())
-        ric_not_available = df['RIC'] == 9
-        bad_mask |= empty_ric
-        bad_mask |= ric_not_available
-    """
-    return ric_df
+    ric[ric == 9] = NaN
+    print("RIC", (ric == 1).sum())
+    ric_permanent = ric_df['RIC_Permanent'].convert_objects(
+        convert_numeric=True)
+    ric_permanent[ric_permanent.isnull()] = 0
+    print("RIC Permanet", (ric_permanent == 1).sum())
+    ric_bad = (ric == 1) & (ric_permanent == 1)
+    return ric_bad
 
 def adverse_events(df):
     """
@@ -298,8 +344,9 @@ def adverse_events(df):
     """
     # convert hemorrhage counts to boolean
     hemorrhage = df['Post GK H'] > 0
-    ric_df = radiation_induced_changes(df)
-    return hemorrhage | ric_df.RIC_Bad
+    print("H", hemorrhage.sum())
+    ric_bad = radiation_induced_changes(df)
+    return hemorrhage | ric_bad
 
 
 def early_ric(df, months=24):
@@ -311,9 +358,9 @@ def early_ric(df, months=24):
     # if this is encoded as 1 in 'RIC', then the time
     # until change is in the column 'RIC post GK'.
     # otherwise, look up their last MRI in 'MR FU'
-    ric = df['RIC'][good].astype(bool)
-    ric_true_time = df['RIC post GK'][good]
-    ric_false_time = df['MR FU'][good]
+    ric = df['RIC'].astype(bool)
+    ric_true_time = df['RIC post GK']
+    ric_false_time = df['MR FU']
     ric_time = ric_true_time.copy()
     ric_false_mask = ~ric
     ric_time[ric_false_mask] = ric_false_time[ric_false_mask]
@@ -321,4 +368,4 @@ def early_ric(df, months=24):
     # cast to a float only once both RIC times and last MRI followup are both in the
     # same series, otherwise we'll get missing values
     ric_time = ric_time.astype(float)
-    return ric & (ric_time <= early_ric_months)
+    return ric & (ric_time <= months)
